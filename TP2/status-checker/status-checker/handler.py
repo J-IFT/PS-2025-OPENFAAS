039c436291153baac78f793327a0d61118dbeef5
J-IFT <juliette.infanti@gmail.com>
2025-06-05 16:39:11 +0200

    fonction 2 et 3 a tester

--- TP2/status-checker/status-checker/handler.py
+++ TP2/status-checker/status-checker/handler.py
@@ -1,5 +1,11 @@
+import os
+import json
+
 def handle(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hello from OpenFaaS!"
-    }
+    folder = "./depot"
+    os.makedirs(folder, exist_ok=True)
+    try:
+        files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+        return json.dumps({"nb_files": len(files)})
+    except Exception as e:
+        return json.dumps({"error": str(e)})
